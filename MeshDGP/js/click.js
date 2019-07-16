4f6786efb8abddd03ee38e875b13cbdae20be556
meshdgp <graphicsresearch@qq.com>
2019-07-16 20:06:16 +0800

    test

---
+++ MeshDGP/js/click.js
@@ -0,0 +1,9 @@
+ function click(value){
+   divGPUContent.style.display ="none";
+    divPrimaryAlgorithemContent.style.display ="none";
+   if(value=='GPU'){
+   divGPUContent.style.display ="block";
+   }else if(value=='Algorithem'){
+   divPrimaryAlgorithemContent.style.display ="block";
+   }
+   }
